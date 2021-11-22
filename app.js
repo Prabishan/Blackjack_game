c729ebed56cb24167f66f5404338135442653665
Prabishan Shrestha <prabishan123@gmail.com>
2021-11-22 15:32:24 -0600

    added dealer logic

--- app.js
+++ app.js
@@ -10,11 +10,13 @@ const player = { 'you': '.player-area', 'bot': '.bot-area' }
 let currPlayer = player['you'];
 let playerScore = 0;
 let botScore = 0;
-
+let pScore = document.querySelector('.player-score');
+let bScore = document.querySelector('.bot-score');
 
 function deal() {
     let playerImg = document.querySelector('.player-area').querySelectorAll('img');
     let botImg = document.querySelector('.bot-area').querySelectorAll('img');
+
 
     for (i = 0; i < playerImg.length; i++) {
         playerImg[i].remove();
@@ -24,11 +26,22 @@ function deal() {
         botImg[i].remove();
     }
     currPlayer = player['you'];
+    playerScore = 0;
+    botScore = 0;
+    pScore.textContent = playerScore;
+    bScore.textContent = botScore;
+
 }
 
 function changePlayertoBot() {
     currPlayer = player['bot'];
-
+    botScore += cardGen(currPlayer);
+    bScore.textContent = botScore;
+    if (botScore > 21) {
+        bScore.textContent = "BUST!!!"
+    } else if (bScore === 21) {
+        alert('You LOSE')
+    }
 }
 function cardGen(playerClass) {
 
@@ -37,12 +50,19 @@ function cardGen(playerClass) {
     cardImg.src = `./images/${randNum}.png`
     cardImg.setAttribute('class', 'cardSize');
     document.querySelector(playerClass).appendChild(cardImg);
+    console.log(randNum);
     return randNum;
 }
 
 function bjHit() {
-
-    let score = cardGen(currPlayer);
+    currPlayer = player['you'];
+    playerScore += cardGen(currPlayer);
+    pScore.textContent = playerScore;
+    if (playerScore > 21) {
+        pScore.textContent = "BUST!!!"
+    } else if (playerScore === 21) {
+        alert('You Winn!!');
+    }
 }
 
 function randomNumGenerator() {
