5f6a5f81a07e3d15f7fd5c969f9aca21b55a95fd
Prabishan Shrestha <prabishan123@gmail.com>
2021-11-22 14:19:39 -0600

    added functionality to button

--- app.js
+++ app.js
@@ -1,10 +1,51 @@
 
 document.getElementById("hit-btn").addEventListener('click', bjHit);
 
-function bjHit() {
+document.getElementById('stand-btn').addEventListener('click', changePlayertoBot);
+
+document.getElementById('deal-btn').addEventListener('click', deal);
+
+const player = { 'you': '.player-area', 'bot': '.bot-area' }
+
+let currPlayer = player['you'];
+let playerScore = 0;
+let botScore = 0;
+
+
+function deal() {
+    let playerImg = document.querySelector('.player-area').querySelectorAll('img');
+    let botImg = document.querySelector('.bot-area').querySelectorAll('img');
+
+    for (i = 0; i < playerImg.length; i++) {
+        playerImg[i].remove();
+    }
+
+    for (i = 0; i < botImg.length; i++) {
+        botImg[i].remove();
+    }
+    currPlayer = player['you'];
+}
+
+function changePlayertoBot() {
+    currPlayer = player['bot'];
+
+}
+function cardGen(playerClass) {
+
     let cardImg = document.createElement('img');
-    cardImg.src = './images/10.png'
-    cardImg.setAttribute('class','cardSize');
-    document.querySelector('.player-area').appendChild(cardImg);
-    
+    let randNum = randomNumGenerator();
+    cardImg.src = `./images/${randNum}.png`
+    cardImg.setAttribute('class', 'cardSize');
+    document.querySelector(playerClass).appendChild(cardImg);
+    return randNum;
+}
+
+function bjHit() {
+
+    let score = cardGen(currPlayer);
+}
+
+function randomNumGenerator() {
+    let rand = Math.floor(Math.random() * 13 + 1);
+    return rand;
 }
